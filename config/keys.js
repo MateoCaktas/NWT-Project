6ca799869346fd982f6e73acd4bd8f108fb01920
Mateo <mcakta00@fesb.hr>
2019-04-11 00:11:41 +0200

    Added Login,JWT,Validation

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://mcakta00:superscription1.@nwtprojektcluster-euawf.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://mcakta00:superscription1.@nwtprojektcluster-euawf.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
 };
