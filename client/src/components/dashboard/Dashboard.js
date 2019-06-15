37a2874a3480214ea6a683594b473f0c33cc3a7c
Mateo <mcakta00@fesb.hr>
2019-06-15 14:20:30 +0200

    Added Education&Experience Forms

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
 import Spinner from "../layout/Spinner";
 import { Link } from "react-router-dom";
+import DashboardActions from "./DashboardActions";
 const Dashboard = ({
   getCurrentProfile,
   auth: { user },
@@ -22,7 +23,10 @@ const Dashboard = ({
         <i className="fas fa-user" /> Welcome {user && user.name}
       </p>
       {profile !== null ? (
-        <Fragment> has </Fragment>
+        <Fragment>
+          {" "}
+          <DashboardActions />{" "}
+        </Fragment>
       ) : (
         <Fragment>
           <p>You have not yet created a profile.</p>{" "}
